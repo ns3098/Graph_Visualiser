f4a8ab1b8ddf67558f5baaa679ffc2d3f5a3b86e
nitin <ns3098@bennett.edu.in>
2020-06-17 13:42:58 +0530

    Api changed

--- OptionDialog/optiondialog.h
+++ OptionDialog/optiondialog.h
@@ -1,24 +1,4 @@
-/*****************************************************************************
- * Copyright © 2015 Oakware - All Rights Reserved
- *
- * Github : <https://github.com/Oakware>
- * Authors: Ivan Kondratuk <i.o.kondratuk@gmail.com>
- *
- * This file is part of Graph Builder.
- *
- * Graph Builder is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Graph Builder is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Graph Builder.  If not, see <http://www.gnu.org/licenses/>.
- *****************************************************************************/
+
 
 #ifndef OPTIONDIALOG_H
 #define OPTIONDIALOG_H
